6261b9390230a9f3bd6256be00ce6522f20b93a6
iSm1le <sm1leua@ya.ru>
2017-04-19 14:09:14 +0300

    v1.0.6, added get-icon command

---
+++ commands/_util/get-icon.js
@@ -0,0 +1,51 @@
+const { Command } = require('discord.js-commando');
+const colors = require('../../assets/_data/colors.json');
+
+module.exports = class SetUserLevelCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'get-icon',
+			aliases: ['gi'],
+			group: 'util',
+			memberName: 'get-icon',
+			description: 'With this command you can get avatar of user or server.',
+
+			args: [
+				{
+					key: 'obj',
+					prompt: 'must be **\'user\'** or **\'server\'** ID\n',
+					type: 'string',
+					default: 'user',
+					validate: obj => {
+						if (['user', 'server'].includes(obj)) {
+							return true;
+						}
+						return `Job name must be **'user'** or **'server'**`;
+					}
+				},
+
+				{
+					key: 'user',
+					prompt: 'please enter a valid user\n',
+					type: 'user',
+					default: ''
+				}
+			]
+		});
+	}
+
+	hasPermission(msg) {
+		return this.client.isOwner(msg.author);
+	}
+
+	async run(msg, args) {
+		const { user } = args;
+		const obj = args.obj === 'user';
+		await msg.embed({
+			color: colors.green,
+			description: `Here **${obj ? user.tag : msg.guild.name}\`s** avatar:`,
+			image: { url: obj ? user.avatarURL : msg.guild.iconURL || undefined }
+		});
+		return null;
+	}
+};
