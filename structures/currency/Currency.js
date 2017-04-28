9eba546711c45b555cca4fc478147882ce018c6e
iSm1le <sm1leua@ya.ru>
2017-04-28 09:57:56 +0300

    v1.0.7, async leaderboard fix

--- structures/currency/Currency.js
+++ structures/currency/Currency.js
@@ -5,8 +5,8 @@ const redis = new Redis();
 
 const UPDATE_DURATION = 30 * 60 * 1000;
 
-redis.db.hgetAsync('money', 'bank').then(balance => {
-	if (!balance) redis.db.hsetAsync('money', 'bank', 5000);
+redis.db.hgetAsync('money', 'bank').then(async balance => {
+	if (!balance) await redis.db.hsetAsync('money', 'bank', 5000);
 });
 
 class Currency {
@@ -65,8 +65,8 @@ class Currency {
 		}
 		/* eslint-enable no-await-in-loop */
 
+		await redis.db.setAsync('moneyleaderboardreset', Date.now());
 		setTimeout(() => Currency.leaderboard(), UPDATE_DURATION);
-		redis.db.setAsync('moneyleaderboardreset', Date.now());
 	}
 
 	static convert(amount, text = false) {
