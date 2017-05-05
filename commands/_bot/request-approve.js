3176a2bcbe9ce206bcd69d7092af5eec3317933d
iSm1le <sm1leua@ya.ru>
2017-05-05 15:44:04 +0300

    v1.0.7, fixed some issues. Added request and issues commands

---
+++ commands/_bot/request-approve.js
@@ -0,0 +1,79 @@
+const { Command } = require('discord.js-commando');
+const colors = require('../../assets/_data/colors.json');
+const Request = require('../../models/Request');
+const { REQUEST_CHANNEL } = process.env;
+
+module.exports = class ApproveRequestCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'request-approve',
+			aliases: ['req-a'],
+			group: 'bot',
+			memberName: 'request-approve',
+			description: 'Approve a requested feature.',
+
+			args: [
+				{
+					key: 'requestID',
+					prompt: 'which feature request do you want to approve?\n',
+					type: 'integer'
+				}
+			]
+		});
+	}
+
+	hasPermission(msg) {
+		return this.client.isOwner(msg.author);
+	}
+
+	async run(msg, { requestID }) {
+		if (msg.channel.id !== REQUEST_CHANNEL) {
+			return msg.embed({
+				color: colors.red,
+				description: `${msg.author}, this command can only be used in the requests channel.`
+			});
+		}
+
+		const request = await Request.findById(requestID);
+		if (!request) {
+			return msg.embed({
+				color: colors.dark_green,
+				description: `${msg.author}, you provided an invalid request id.`
+			});
+		}
+		await request.update({
+			processed: true,
+			processedBy: msg.author.id,
+			approved: true
+		});
+
+		await this.client.users.get(request.requester).send({
+			embed: {
+				color: colors.green,
+				author: {
+					name: 'Request approved',
+					icon_url: msg.author.displayAvatarURL // eslint-disable-line camelcase
+				},
+				description: 'Your request has been reviewed and approved!',
+				fields: [
+					{
+						name: 'Your request:',
+						value: request.request.length <= 1024 ? request.request : `${request.request.substr(0, 1021)}...`
+					}
+				]
+			}
+		});
+
+		return msg.embed({
+			color: colors.green,
+			description: `${msg.author}, successfully approved request #${request.id}!`
+		})
+			.then(async () => {
+				const messages = await msg.channel.fetchMessages({ after: msg.id });
+				const requestMessage = await msg.channel.fetchMessage(request.requestMessage);
+				messages.deleteAll();
+				msg.delete();
+				requestMessage.delete();
+			});
+	}
+};
---
+++ models/Request.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+
+const Database = require('../structures/PostgreSQL');
+
+const Request = Database.db.define('requests', {
+	requester: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	request: {
+		type: Sequelize.STRING(2000), // eslint-disable-line new-cap
+		allowNull: false
+	},
+	requestMessage: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+	processed: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false
+	},
+	processedBy: {
+		type: Sequelize.STRING,
+		allowNull: true
+	},
+	approved: {
+		type: Sequelize.BOOLEAN,
+		allowNull: true
+	},
+	reason: {
+		type: Sequelize.STRING(2000), // eslint-disable-line new-cap
+		allowNull: true
+	}
+});
+
+module.exports = Request;
